a7dd7019a11953d208ddc261b36c2a942a4a69bf
gabriel <gabrielmatheusdesouza26@gmail.com>
2024-11-02 13:47:23 -0300

    A

---
+++ A/museum_night.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_SIZE 100
+
+int mod(int n);
+
+int main() {
+  int rotations, iterator, diff, half;
+  char str[MAX_SIZE + 1], previous;
+
+  scanf("%100[^\n]", str);
+
+  half = 13;
+  previous = 'a';
+  iterator = rotations = 0;
+
+  while (str[iterator] != '\0') {
+    diff = mod(str[iterator] - previous);
+
+    if (diff > half)
+      diff = 26 - diff;
+
+    previous = str[iterator];
+    rotations += diff;
+    iterator++;
+  }
+
+  printf("%d", rotations);
+  return 0;
+}
+
+int mod(int n) {
+  if (n < 0)
+    n = -n;
+
+  return n;
+}
